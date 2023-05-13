4b0da90826a6c69ae03ef8d447b0b216304cf46e
omvastrad33 <105169000+omvastrad33@users.noreply.github.com>
2023-05-14 02:32:19 +0530

    Add files via upload

---
+++ greedy2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int coins[] = {1, 5, 10, 25};
+    int n = sizeof(coins) / sizeof(coins[0]);
+
+    int value = 67;
+
+    printf("Minimum number of coins required to make %d is %d\n", value, minCoins(coins, n, value));
+
+    return 0;
+}
+
+int minCoins(int coins[], int n, int value)
+{
+    int count = 0;
+
+    for (int i = n - 1; i >= 0; i--) {
+
+        while (value >= coins[i]) {
+            value -= coins[i];
+            count++;
+        }
+    }
+
+    return count;
+}
+
